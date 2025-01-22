c9fba6b9186ae49c1a9b6059a47722d7ee7c85ff
Markus Kurbel <m4rku@pm.me>
2025-01-22 06:47:50 +0100

    Add due date and priority

--- main.go
+++ main.go
@@ -6,14 +6,38 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
+
+type Priority int
+
+const (
+	Low Priority = iota
+	Medium
+	High
+)
+
+func (p Priority) String() string {
+	switch p {
+	case Low:
+		return "Low"
+	case Medium:
+		return "Medium"
+	case High:
+		return "High"
+	default:
+		return "Unknown"
+	}
+}
 
 type ToDo struct {
 	ID          int
 	Title       string
 	CreatedAt   time.Time
 	CompletedAt time.Time `json:",omitempty"`
+	DueDate     time.Time `json:",omitempty"`
+	Priority    Priority  `json:",omitempty"`
 	Completed   bool
 }
 
@@ -104,18 +128,54 @@ func readLine() string {
 	return scanner.Text()
 }
 
+func findUniqueID() int {
+	uniqueID := 0
+	for _, todo := range todos {
+		if todo.ID > uniqueID {
+			uniqueID = todo.ID
+		}
+	}
+	return uniqueID + 1
+}
+
 func addToDo() {
 	fmt.Println("Add")
 	fmt.Println("Enter the task:")
 	title := readLine()
 
+	fmt.Println("Enter the due date (YYYY-MM-DD):")
+	dueDate := readLine()
+
+	parsedDueDate, err := time.Parse("2006-01-02", dueDate)
+	if err != nil {
+		fmt.Println("Invalid due date, defaulting to no due date")
+		parsedDueDate = time.Time{}
+	}
+	if parsedDueDate.Before(time.Now().AddDate(0, 0, -1)) {
+		fmt.Println("Due date must be in the future, defaulting to no due date")
+		parsedDueDate = time.Time{}
+	}
+
+	fmt.Println("Enter the priority (1-3) (1: Low, 2: Medium, 3: High) (default: 1)")
+	priorityStr := readLine()
+	priorityNum, _ := strconv.Atoi(priorityStr)
+	priorityNum--
+
+	if priorityNum < 0 || priorityNum > 2 {
+		fmt.Println("Invalid priority, defaulting to Low")
+		priorityNum = 0
+	}
+
 	todo := ToDo{
-		ID:        len(todos) + 1,
+		ID:        findUniqueID(),
 		Title:     title,
 		CreatedAt: time.Now(),
+		DueDate:   parsedDueDate,
+		Priority:  Priority(priorityNum),
 		Completed: false,
 	}
 	todos = append(todos, todo)
+	fmt.Println("Task added successfully")
 }
 
 func deleteToDo(id int) {
@@ -134,11 +194,19 @@ func listToDo() {
 	fmt.Println("Your ToDo List:")
 	if len(todos) == 0 {
 		fmt.Println("No tasks found")
+		fmt.Println("--------------------------------")
 		return
 	}
 	for _, todo := range todos {
 		if !todo.Completed {
 			fmt.Printf("%d. %s\n", todo.ID, todo.Title)
+			indent := strings.Repeat(" ", len(strconv.Itoa(todo.ID))+2)
+			if !todo.DueDate.IsZero() {
+				fmt.Printf("%sDue: %s\n", indent, todo.DueDate.Format("2006-01-02"))
+			} else {
+				fmt.Printf("%sDue: %s\n", indent, "No due date")
+			}
+			fmt.Printf("%sPriority: %s\n", indent, todo.Priority)
 		}
 	}
 	fmt.Println("--------------------------------")
@@ -149,6 +217,8 @@ func listToDo() {
 		if todo.Completed {
 			hasCompleted = true
 			fmt.Printf("%d. %s\n", todo.ID, todo.Title)
+			indent := strings.Repeat(" ", len(strconv.Itoa(todo.ID))+2)
+			fmt.Printf("%sCompleted: %s\n", indent, todo.CompletedAt.Format("2006-01-02"))
 		}
 	}
 	if !hasCompleted {
