565108f57f52bcaeb6701ae6755d9bd0d1896a1e
Markus Kurbel <m4rku@pm.me>
2025-02-02 17:56:51 +0100

    Add basic textinput

--- autocomplete/main.go
+++ autocomplete/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"autocomplete/ollamastream"
 	"context"
 	"fmt"
 	"log"
 	"time"
+
+	"autocomplete/ollamastream"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-func generate(prompt string) {
+func generate(prompt string) string {
 	const ollamaEndpoint = "http://localhost:11434/api/generate"
 	var fullText string
 
@@ -19,21 +23,78 @@ func generate(prompt string) {
 		ctx,
 		prompt,
 		ollamaEndpoint,
-		"deepseek-r1",
+		"llama3.2",
 		0.7,
 		80,
 		func(token string) {
 			fullText += token
-			fmt.Print(token)
 		},
 	)
 	if err != nil {
 		log.Fatalf("GenerateStream error: %v", err)
 	}
 
-	fmt.Printf("\n\n---\nFull text:\n%s\n", fullText)
+	return fullText
 }
 
 func main() {
-	generate("Write a short poem about autumn leaves.")
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type (
+	errMsg error
+)
+
+type model struct {
+	textInput textinput.Model
+	err       error
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = ""
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
+	return model{
+		textInput: ti,
+		err:       nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		}
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textInput.Placeholder = generate(m.textInput.Value())
+	fmt.Println(m.textInput.Placeholder)
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"Let AI autocomplete your text\n\n%s\n\n%s",
+		m.textInput.View(),
+		"(esc to quit)",
+	) + "\n"
 }
